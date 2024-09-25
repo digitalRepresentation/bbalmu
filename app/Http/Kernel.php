ed77e9972a091f6d2c5f28b234cc418c559665d4
DESKTOP-BEKN6N9\ちなつ <kinemura@naver.com>
2024-09-25 20:15:20 +0900

    라라벨9버전 커널로 미들웨어 설정

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check.notice.owner' => \App\Http\Middleware\CheckNoticeOwner::class,
     ];
 }
