37fd922c716567713eff56ca4ea83d16173feb60
DESKTOP-BEKN6N9\ちなつ <kinemura@naver.com>
2024-09-25 20:20:47 +0900

    elo시스템 페이지 제작

--- resources/views/elo/stats.blade.php
+++ resources/views/elo/stats.blade.php
@@ -6,7 +6,7 @@
 <body>
 @include('layouts.header')
 
-<h4>제작관련자 : 3040-ChanWoo, 3040-Wasabi, 3040-NGPark 
+<h4>제작관련자 : 3040-ChanWoo, 3040-Wasabi, 3040-NGPark</h4>
 
 <iframe width="100%" height="1000" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSSsyWn4I_vhEgzu9HF5soUgKWVe2BMMDovzOvNhj_mooDzpvWCbB-vxo6gJxH5zrGeXgUf0d4lUjOy/pubhtml?gid=659908496&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
 
