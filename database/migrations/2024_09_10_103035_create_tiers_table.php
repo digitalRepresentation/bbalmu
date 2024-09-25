4c33ab0f4ef06463d5f1dcb0dfc58985bce0ff68
DESKTOP-BEKN6N9\ちなつ <kinemura@naver.com>
2024-09-25 20:20:34 +0900

    티어페이지 제작

--- database/migrations/2024_09_10_103035_create_tiers_table.php
+++ database/migrations/2024_09_10_103035_create_tiers_table.php
@@ -15,7 +15,18 @@ return new class extends Migration
     {
         Schema::create('tiers', function (Blueprint $table) {
             $table->id();
+            $table->string('starcraft_name');
+            $table->integer('tier');
+            $table->string('starcraft_race');
+            $table->integer('ladder_score')->default(1000);
             $table->timestamps();
+
+            //배틀넷 이름
+            //프,테,저 종족 -> 전적 테이블
+            //
+            //종족
+            //래더점수
+
         });
     }
 
